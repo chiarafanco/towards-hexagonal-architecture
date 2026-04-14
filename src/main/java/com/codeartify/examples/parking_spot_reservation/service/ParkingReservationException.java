fa0ccb6b87e3ca029cb5c252204d8ed7bb654ae9
chiara.fanco <chiara.fanco@immobiliare.it>
2026-04-14 18:24:32 +0200

    Add ParkingReservationException

--- src/main/java/com/codeartify/examples/parking_spot_reservation/service/ParkingReservationException.java
+++ src/main/java/com/codeartify/examples/parking_spot_reservation/service/ParkingReservationException.java
@@ -1,4 +1,14 @@
 package com.codeartify.examples.parking_spot_reservation.service;
 
-public class ParkingReservationException extends RuntimeException {
+public abstract sealed class ParkingReservationException extends RuntimeException {
+
+    public static final class AlreadyExists extends ParkingReservationException {}
+
+    public static final class PeriodEndBeforeStart extends ParkingReservationException {}
+
+    public static final class PeriodOutsideOperatingHours extends ParkingReservationException {}
+
+    public static final class PeriodTooShort extends ParkingReservationException {}
+    
+    public static final class SpotUnavailable extends ParkingReservationException {}
 }
