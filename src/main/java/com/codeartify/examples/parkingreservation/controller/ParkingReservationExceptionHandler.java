afc0439dd3cd79b68358e1aeecfd4c651dbe77e6
chiara.fanco <chiara.fanco@immobiliare.it>
2026-04-16 12:55:37 +0200

    Add problem detail

--- src/main/java/com/codeartify/examples/parkingreservation/controller/ParkingReservationExceptionHandler.java
+++ src/main/java/com/codeartify/examples/parkingreservation/controller/ParkingReservationExceptionHandler.java
@@ -2,28 +2,25 @@ package com.codeartify.examples.parkingreservation.controller;
 
 import com.codeartify.examples.parkingreservation.service.ParkingReservationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 class ParkingReservationExceptionHandler {
     
     @ExceptionHandler
-    @ResponseStatus(HttpStatus.CONFLICT)
-    String handleException(ParkingReservationException.AlreadyExistsException e) {
-        return e.getMessage();
+    ProblemDetail handleException(ParkingReservationException.AlreadyExistsException e) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, e.getMessage());
     }
 
     @ExceptionHandler
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    String handleException(ParkingReservationException.SpotUnavailableException e) {
-        return e.getMessage();
+    ProblemDetail handleException(ParkingReservationException.SpotUnavailableException e) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.getMessage());
     }
 
     @ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    String handleException(ParkingReservationException e) {
-        return e.getMessage();
+    ProblemDetail handleException(ParkingReservationException e) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, e.getMessage());
     }
 }
